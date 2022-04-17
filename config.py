b2419fce309574c62ebb8edac0a9b2fa174a46e2
Hehahei <h1092853954@outlook.com>
2022-04-17 11:11:25 +0800

    更新配置

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ baseDir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = "temp"
 
 PORT = "8887"
+
+LOG_PATH = os.path.join(baseDir, "logs")
--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from app import app
-import logging
+import logging, os
 from logging.handlers import TimedRotatingFileHandler
-from config import PORT
+from config import PORT, LOG_PATH
 
+
+if not os.path.exists(LOG_PATH):
+        os.mkdir(LOG_PATH)
 
 app.logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
         "[%(asctime)s]-[%(module)s:%(lineno)d]-[%(levelname)s]-[%(thread)d] - %(message)s")
-handler = TimedRotatingFileHandler("flask.log", when="D", interval=1, backupCount=15,
+handler = TimedRotatingFileHandler(os.path.join(LOG_PATH, "flask.log"), when="D", interval=1, backupCount=15,
         encoding="UTF-8", delay=False, utc=True)
 handler.setLevel(logging.INFO)
 
